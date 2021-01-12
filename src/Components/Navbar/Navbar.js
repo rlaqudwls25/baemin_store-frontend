2acd82841cf2a67b1a670b0a104d22a54de5e8bc
Taeng <dyek72731@gmail.com>
2021-01-12 18:38:43 +0900

    Modify: Navbar 반복메뉴 NavbarData.js 데이터화 및 map처리

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { HEADERLINK, FOOTERLINK } from './NavbarData'
 import './Navbar.scss'
 
 export default class Navbar extends Component {
@@ -10,20 +11,14 @@ export default class Navbar extends Component {
         <header>
           <div className="nav-header-wrap">
             <ul>
-              <li key="login">
-                <Link to="/">로그인</Link>
-              </li>
-              <li key="register">
-                <Link to="/">회원가입</Link>
-              </li>
-              <li key="mypage">
-                <Link to="/">마이페이지</Link>
-              </li>
-              <li key="cart" class="nav-cart">
-                <Link to="/">
-                  장바구니<span className="cart-count">0</span>
-                </Link>
-              </li>
+              {HEADERLINK.map(linkInfo => 
+                <li key={linkInfo.key} class={`nav-${linkInfo.key}`}>
+                  <Link to={linkInfo.link}>
+                    {linkInfo.content}
+                    {linkInfo.key === 'cart' && <span className="cart-count">0</span>}
+                  </Link>
+                </li>  
+              )}
             </ul>
           </div>
         </header>
@@ -36,12 +31,11 @@ export default class Navbar extends Component {
         </section>
         <footer>
           <ul>
-            <li key="category-all"><Link to="/">전체</Link></li>
-            <li key="category-stationary"><Link to="/">문구</Link></li>
-            <li key="category-living"><Link to="/">리빙</Link></li>
-            <li key="category-book"><Link to="/">책</Link></li>
-            <li key="category-bmfriends"><Link to="/">배달이친구들</Link></li>
-            <li key="category-giftset"><Link to="/">선물세트</Link></li>
+            {FOOTERLINK.map(linkInfo => 
+              <li key={linkInfo.key}>
+                <Link to={linkInfo.link}>{linkInfo.content}</Link>
+              </li>  
+            )}
           </ul>
         </footer>
       </nav>
---
+++ src/Components/Navbar/NavbarData.js
@@ -0,0 +1,66 @@
+export const HEADERLINK = [
+  {
+    id: 0,
+    key: 'login',
+    content: '로그인',
+    link: '/',
+  },
+  {
+    id: 1,
+    key: 'register',
+    content: '회원가입',
+    link: '/',
+  },
+  {
+    id: 2,
+    key: 'mypage',
+    content: '마이페이지',
+    link: '/',
+  },
+  {
+    id: 3,
+    key: 'cart',
+    content: '장바구니',
+    link: '/',
+  },
+]
+
+export const FOOTERLINK = [
+  {
+    id: 0,
+    key: 'all',
+    content: '전체',
+    link: '/',
+  },
+  {
+    id: 1,
+    key: 'stationary',
+    content: '문구',
+    link: '/',
+  },
+  {
+    id: 2,
+    key: 'living',
+    content: '리빙',
+    link: '/',
+  },
+  {
+    id: 3,
+    key: 'book',
+    content: '책',
+    link: '/',
+  },
+  {
+    id: 4,
+    key: 'bfriends',
+    content: '배달이친구들',
+    link: '/',
+  },
+  {
+    id: 5,
+    key: 'giftset',
+    content: '선물세트',
+    link: '/',
+  },
+
+]
