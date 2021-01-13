cb6c61410fb9bbf6c5333fb722c8990f091d2278
rlaqudwls157 <rlaqudwls157@gmail.com>
2021-01-13 21:07:25 +0900

    modify: index.js 수정

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Routes from './Routes';
+import './Styles/reset.scss';
+import './Styles/common.scss';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Routes />
   </React.StrictMode>,
   document.getElementById('root')
 );
