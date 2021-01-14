a691a26ecdfba6506d57e6a982f8deb76475f94b
rlaqudwls157 <rlaqudwls157@gmail.com>
2021-01-14 21:48:11 +0900

    상품 상세페이지 초기 셋팅 완료

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer'
+import Detail from './Pages/Detail/Detail';
 
 class Routes extends Component {
   render() {
@@ -10,6 +11,7 @@ class Routes extends Component {
         <Navbar />
         <Switch>
           <Route exact path="/" />
+          <Route exact path="/detail" component={Detail} />
         </Switch>
         <Footer />
       </Router>
