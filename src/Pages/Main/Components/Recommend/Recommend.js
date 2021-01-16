d34dc03e75673490e05b76ed95b675df6761d6aa
Taeng <dyek72731@gmail.com>
2021-01-16 16:45:22 +0900

    Add: Slider, SearchBar 추가예정

--- src/Pages/Main/Components/Recommend/Recommend.js
+++ src/Pages/Main/Components/Recommend/Recommend.js
@@ -6,7 +6,7 @@ export default class Recommend extends Component {
     return (
       <article className="recommend-item">
         <a href="/">
-          <img alt="recommend-item-img" src="./Images/default.png" />
+          <img alt="recommend-item-img" src="/Images/default.png" />
           <div>
             <p className="recommend-item-title">그래, 결심했어!</p>
             <p className="recommend-item-desc">레터링시트지</p>
